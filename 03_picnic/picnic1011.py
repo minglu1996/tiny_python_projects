0c0db4a1cc59cafdbbc9fc923e604b33b1b9e1e0
ml-john <mingluxie@gmail.com>
2022-10-12 13:55:14 +0200

    Charpter 3 - bad methods

--- 03_picnic/picnic1011.py
+++ 03_picnic/picnic1011.py
@@ -7,6 +7,8 @@ Purpose: Picnic game
 
 import argparse
 # --------------------------------------------------
+
+
 def get_arg():
     """Get command-line arguments"""
     parser = argparse.ArgumentParser(
@@ -17,7 +19,7 @@ def get_arg():
                         metavar='str',
                         nargs='+',
                         help='Item(s) to bring'
-    )
+                        )
     parser.add_argument('-s',
                         '--sorted',
                         action="store_true",
@@ -26,6 +28,8 @@ def get_arg():
     return parser.parse_args()
 
 # --------------------------------------------------
+
+
 def main():
     args = get_arg()
     item = args.item
@@ -39,9 +43,14 @@ def main():
     elif num == 2:
         print("You are bringing {} and {}.".format(item[0], item[1]))
     elif num == 3:
-        print("You are bringing {}, {}, and {}.".format(item[0],item[1],item[2]))
+        print(
+            "You are bringing {}, {}, and {}.".format(
+                item[0],
+                item[1],
+                item[2]))
     else:
-        print("You are bringing {}, {}, {}, and {}.".format(item[0],item[1],item[2],item[-1]))
+        print("You are bringing {}, {}, {}, and {}.".format(
+            item[0], item[1], item[2], item[-1]))
 
 
 # --------------------------------------------------
